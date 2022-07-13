64511fc19120ec2d12abb44b7da2848caea81ac0
bucr <whut_bcr@163.com>
2022-07-14 00:56:18 +0800

    add vue-router@4

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from "vue";
 import ElementPlus from "element-plus";
+import router from "./router";
 import "element-plus/dist/index.css";
 import App from "./App.vue";
 
 const app = createApp(App);
 
-app.use(ElementPlus);
+app.use(ElementPlus).use(router);
 app.mount("#app");
